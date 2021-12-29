02eecc28c6729723ed4da5a9602e81ca93cc1123
vineykhera <vineykhera@gmail.com>
2021-12-29 08:45:15 -0500

    Time: 208 ms (5.01%), Space: 14.7 MB (29.49%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.py
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.py
@@ -1,45 +1,44 @@
-# class Solution:
-#     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
-#         # start = 0
-#         # end = 1
-#         # mid = (end-start)//2 + start
-#         # print(mid)
-#         start = 0
-#         end = len(letters) -1
-#         while start < end:
-#             mid = (end-start)//2 + start
-#             if letters[mid] > target:
-#                 end = mid 
-#                 # break
-#             else:
-#                 start = mid + 1
-      
-#         return letters[((start) % len(letters))]
-
 class Solution:
     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
-        letters_length = len(letters)
-        low = 0
-        high = letters_length - 1
-        
-		# target is outside the bounds of letters
-		# because the array is circular, this means that the next greatest value 
-		# must be the first value in the array. If the value is within the bounds
-		# of the array we can perform our regular modified binary search
-        if target < letters[low] or target >= letters[high]:
-            return letters[low]
+        # start = 0
+        # end = 1
+        # mid = (end-start)//2 + start
+        # print(mid)
+        start = 0
+        end = len(letters) -1
+        while start <= end:
+            mid = (end-start)//2 + start
+            if target >= letters[mid]:
+                start = mid + 1 
+            else:
+                end = mid - 1
+      
+        return letters[((start) % len(letters))]
 
-        while low <= high:
-            middle =  (low + high) // 2
-            candidate = letters[middle]
-            
-            if target < candidate: 
-                high = middle - 1
-            
-			# becuase we're looking for the smallest value thats greater then the target
-			# we can condense the typical case where target == letters[middle] into the 
-			# the target > letters[middle] case
-            if target >= candidate :
-                low = middle + 1
+# class Solution:
+#     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+#         letters_length = len(letters)
+#         low = 0
+#         high = letters_length - 1
         
-        return letters[low]
+# 		# target is outside the bounds of letters
+# 		# because the array is circular, this means that the next greatest value 
+# 		# must be the first value in the array. If the value is within the bounds
+# 		# of the array we can perform our regular modified binary search
+#         if target < letters[low] or target >= letters[high]:
+#             return letters[low]
+
+#         while low <= high:
+#             middle =  (low + high) // 2
+#             candidate = letters[middle]
+            
+#             if target < candidate: 
+#                 high = middle - 1
+            
+# 			# becuase we're looking for the smallest value thats greater then the target
+# 			# we can condense the typical case where target == letters[middle] into the 
+# 			# the target > letters[middle] case
+#             if target >= candidate :
+#                 low = middle + 1
+        
+#         return letters[low]
