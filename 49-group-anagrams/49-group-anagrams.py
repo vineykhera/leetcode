abbc1d39be380dddc95ae78b5ea58849ac9aa2d6
vineykhera <vineykhera@gmail.com>
2022-01-11 07:00:00 -0500

    Time: 111 ms (50.57%), Space: 18.2 MB (34.48%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.py
@@ -0,0 +1,17 @@
+"""
+seen - e a t b a
+
+
+
+
+
+"""
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dic = collections.defaultdict(list)
+        for s in strs:
+            dic[tuple(sorted(c for c in s))].append(s)
+        
+        
+        return [values for values in dic.values()]
