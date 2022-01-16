b7b03345e44c26433768e280acdf85a59c3d6017
vineykhera <vineykhera@gmail.com>
2022-01-16 07:07:11 -0500

    Time: 68 ms (13.60%), Space: 14.5 MB (44.17%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,21 +1,49 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        result = []
+   
+        result = list()
         
-        if (len(nums) == 1):
-            return [nums[:]]
+        # base case -- least valid input
+        if len(nums) == 1:
+            return [nums]
         
-        for _ in range(len(nums)):
-            n = nums.pop(0)
-            perms = self.permute(nums)
-       
-            for perm in perms:
-                perm.append(n) # [2,3]+[1] and [3,2]+[1] individually
-            result.extend(perms) # [2,3,1],[3,2,1] all together into the result
-            nums.append(n)
-        return result
- 
+        for idx in range(len(nums)):
+            
+            # compute the permutation for ith element
+            current_nums = nums[:idx] + nums[idx+1:]
+            
+            # recursive case
+            perms = self.permute(current_nums)
 
+            # permutation generated above don't have the 
+            # ith element, so append it
+            for perm in perms:
+                perm.append(nums[idx])
+                
+            
+            # update the overall results with all (multiple) permutations
+            result.extend(perms)
+            
+        #return all permutations
+        return result    
+
+
+#     def permute(self, nums: List[int]) -> List[List[int]]:
+#         result = []
+        
+#         if (len(nums) == 1):
+#             return [nums[:]]
+        
+#         for _ in range(len(nums)):
+#             n = nums.pop(0)
+#             perms = self.permute(nums)
+       
+#             for perm in perms:
+#                 perm.append(n) # [2,3]+[1] and [3,2]+[1] individually
+#             result.extend(perms) # [2,3,1],[3,2,1] all together into the result
+#             nums.append(n)
+#         return result
+ 
 
 # class Solution:
 #     def permute(self, nums: List[int]) -> List[List[int]]:
@@ -35,30 +63,3 @@ class Solution:
 #         return result
 
 
-#     def permute(self, nums: List[int]) -> List[List[int]]:
-   
-#     result = list()
-        
-#         # base case -- least valid input
-#         if len(nums) == 1:
-#             return [nums]
-        
-#         for idx in range(len(nums)):
-            
-#             # compute the permutation for ith element
-#             current_nums = nums[:idx] + nums[idx+1:]
-            
-#             # recursive case
-#             perms = self.permute(current_nums)
-
-#             # permutation generated above don't have the 
-#             # ith element, so append it
-#             for perm in perms:
-#                 perm.append(nums[idx])
-                
-            
-#             # update the overall results with all (multiple) permutations
-#             result.extend(perms)
-            
-#         #return all permutations
-#         return result    
