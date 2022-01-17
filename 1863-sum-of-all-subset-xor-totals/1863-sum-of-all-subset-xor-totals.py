4eccc8854b7333a7ead6c5d9c07e6b48449bcb94
vineykhera <vineykhera@gmail.com>
2022-01-17 12:15:03 -0500

    Time: 340 ms (5.02%), Space: 14.2 MB (78.39%) - LeetHub

---
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
@@ -0,0 +1,50 @@
+
+"""
+7:41
+7:47
+                      5,1,6
+            5,1      1,6          5,6
+         5     1     1    6       5    6
+                
+"""
+class Solution:
+    def subsetXORSum(self, nums: List[int]) -> int:
+        result =list()
+        self.output = 0
+        
+        def subsets(start, result):
+            cursum = 0
+            print("Subset", result)
+            for n in result:
+                cursum ^= n
+            self.output += cursum
+
+            for i in range(start,len(nums)):
+                result.append(nums[i])
+                subsets(i+1, result)
+                result.pop()
+            
+        subsets(0, result)
+        # print(result)
+        return self.output
+    
+    
+
+#         self.res = 0
+        
+#         def dfs(path, start):
+#             curSum = 0
+#             print(path)
+#             for n in path:
+#                 curSum = curSum ^ n
+#             self.res += curSum
+            
+#             for i in range(start, len(nums)):
+#                 num = nums[i]
+#                 path.append(num)
+#                 dfs(path, i + 1)
+#                 path.pop()
+            
+#         dfs([], 0)    
+#         return self.res
+
