16ef67dc5ee5600d55580e9d693449cd4391ca17
vineykhera <vineykhera@gmail.com>
2022-01-18 21:56:52 -0500

    Time: 130 ms (27.87%), Space: 14.4 MB (52.31%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.py
@@ -0,0 +1,30 @@
+"""
+stop if exceed target
+reuse number > 0
+= target return
+
+
+                 2,3,6,7
+        2       3        6       7
+        
+
+
+
+"""
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        def backtrack(strt, path):
+            if sum(path) == target:
+                res.append(path[:])
+                # strt += 1
+            if sum(path) < target:
+                for i in range(strt, len(candidates)):
+                    if sum(path)+candidates[i] <= target:
+                        backtrack(i, path+[candidates[i]])
+        
+        strt = 0
+        candidates.sort()    
+        backtrack(strt, [])
+        return res
