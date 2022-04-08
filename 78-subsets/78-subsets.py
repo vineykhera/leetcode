ec36f8e999026e1748fdaea773fe3c176f294f64
vineykhera <vineykhera@gmail.com>
2022-04-07 23:01:33 -0400

    Time: 47 ms (54.13%), Space: 14 MB (80.55%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,21 +1,48 @@
 """
-loop
-array/str value append
-recuirse  - adds to output array
-pop that value appeneded
+       []
+   [1]      []
+ [1,2] [1]    []
+
+cur
+helper(cur, nums)
+base conditon
+set(outarr size) = 2 ^ n 
+
+for loop 
+choice
+recurse helper ( curr+1)
+undo my choice
+
+def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        self.backtracking(res,0,[],nums)
+        return res
+    
+    def backtracking(self,res,start,subset,nums):
+        res.append(list(subset))
+        for i in range(start,len(nums)):
+            subset.append(nums[i])
+            self.backtracking(res,i+1,subset,nums)
+            subset.pop()
 
 """
 
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        output = []
-        cur = []
         
-        def helper(nums, cur, output):
-            output.append(cur)
-            for i,n in enumerate(nums):                
-                helper(nums[i+1:], cur + [nums[i]], output)
-
-        helper(nums, cur, output)                
-        return output            
-                
+        out = []
+        
+        def helper(nums, path, cur):
+            # print("path", path)
+            if cur == len(nums):
+                out.append(path)
+                return out
+            
+            # for i in range(cur, len(nums)):
+            helper(nums, path+[nums[cur]],cur+1)
+            helper(nums, path, cur+1)
+            
+            
+        helper(nums, [], 0)
+        return out
+        
