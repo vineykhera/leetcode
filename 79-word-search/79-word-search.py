6b60021a737426a3fd90ac0d1ee5c630906a5fda
vineykhera <vineykhera@gmail.com>
2022-04-08 00:29:22 -0400

    Time: 6968 ms (57.38%), Space: 14 MB (54.89%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -7,59 +7,59 @@ wrong
 
 """
 class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool:
-            rows, cols = len(board), len(board[0])
-            path = set()
-
-            def dfs(r, c, i):
-                if i == len(word):
-                    return True
-                if r < 0 or r >= rows or c <  0 or c >= cols or board[r][c] != word[i] or (r,c) in path:
-                    return False
-
-                path.add((r,c))
-                res = dfs(r + 1, c, i+1) or dfs(r - 1, c, i+1) or dfs(r, c + 1, i+1) or dfs(r , c - 1, i + 1)
-                path.remove((r,c))
-                return res
-
-            for r in range(rows):
-                for c in range(cols):
-                    if dfs(r, c, 0): return True
-
-            return False
-    
 #     def exist(self, board: List[List[str]], word: str) -> bool:
-#         r = len(board)
-#         c = len(board[0])
-#         visited = set()
-        
-#         dircs = [(1,0), (-1,0), (0,1), (0,-1)]
-        
-#         def isvalidcord(r,c,i,j,word, wordindx):
-#             if i >= 0 and i < r and j >= 0 and j < c and (i,j) not in visited and board[i][j] == word[wordindx]:
-#                 return True
-#             else:
-#                 return False
-        
-#         def helper(board, r,c, i, j, word, wordindx):
-#             # print(wordindx)
-#             if wordindx == len(word):
-#                 return True
+#             rows, cols = len(board), len(board[0])
+#             path = set()
 
-#             if i < 0 or i >= r or j < 0 or j >= c or (i,j) in visited or board[i][j] != word[wordindx]:
-#                 return False
+#             def dfs(r, c, i):
+#                 if i == len(word):
+#                     return True
+#                 if r < 0 or r >= rows or c <  0 or c >= cols or board[r][c] != word[i] or (r,c) in path:
+#                     return False
+
+#                 path.add((r,c))
+#                 res = dfs(r + 1, c, i+1) or dfs(r - 1, c, i+1) or dfs(r, c + 1, i+1) or dfs(r , c - 1, i + 1)
+#                 path.remove((r,c))
+#                 return res
+
+#             for r in range(rows):
+#                 for c in range(cols):
+#                     if dfs(r, c, 0): return True
+
+#             return False
+    
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        r = len(board)
+        c = len(board[0])
+        visited = set()
+        
+        dircs = [(1,0), (-1,0), (0,1), (0,-1)]
+        
+        def isvalidcord(r,c,i,j,word, wordindx):
+            if i >= 0 and i < r and j >= 0 and j < c and (i,j) not in visited and board[i][j] == word[wordindx]:
+                return True
+            else:
+                return False
+        
+        def helper(board, r,c, i, j, word, wordindx):
+            # print(wordindx)
+            if wordindx == len(word):
+                return True
+
+            if i < 0 or i >= r or j < 0 or j >= c or (i,j) in visited or board[i][j] != word[wordindx]:
+                return False
             
-#             visited.add((i,j))
-#             for x,y in dircs:
-#                 if helper(board, r,c,i+x,j+y, word, wordindx+1):
-#                     return True
-#             visited.remove((i,j))
-#             # return False
+            visited.add((i,j))
+            for x,y in dircs:
+                if helper(board, r,c,i+x,j+y, word, wordindx+1):
+                    return True
+            visited.remove((i,j))
+            # return False
         
-#         for i in range(r):
-#             for j in range(c):
-#                 if helper(board, r,c,i,j, word, 0):
-#                     return True
+        for i in range(r):
+            for j in range(c):
+                if helper(board, r,c,i,j, word, 0):
+                    return True
         
-#         return False
+        return False
                     
