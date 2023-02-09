5e9fb56fd0d375e493cf7ea6eb1244223d4f8423
vineykhera <vineykhera@gmail.com>
2023-02-09 10:32:35 -0500

    Time: 72 ms (96.59%), Space: 17.3 MB (33.24%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,32 @@
+"""
+1,3
+2,2
+3,1
+
+push to min heap of size k
+if len(heap) becomes > k then pop 
+
+
+"""
+
+import heapq
+
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        heaparr = []
+        cntnums = collections.Counter(nums)
+        heaparr = [(cnt,n) for n, cnt in cntnums.items()]
+        
+        heapq.heapify(heaparr)
+
+        for i in range(len(heaparr) - k):
+            if len(heaparr) > k:
+                heapq.heappop(heaparr)
+        
+        return [y for x,y in heaparr]
+        
